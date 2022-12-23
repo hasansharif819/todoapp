c5e2fdaf2227fbc2b0eb1512cee6a7c6ef6486ce
hasansharif819 <hs.sharif819@gmail.com>
2022-12-24 01:27:25 +0600

    todo reducer

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,13 @@
+export const initialState = [
+    {
+        id: 1,
+        text: "Learning on Drupal",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Learning on Django",
+        completed: false,
+        color: "red"
+    }
+]
