0fdf82f7a4dd0d9346bd6f355d7b04d5dd9fe383
hasansharif819 <hs.sharif819@gmail.com>
2022-12-25 20:59:08 +0600

    completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,43 +2,71 @@ import React from 'react';
 import notes from '../images/notes.png';
 import plus from '../images/plus.png';
 import tick from '../images/double-tick.png';
+import { useState } from 'react';
+import {added, allCompleted, clearCompleted} from '../redux/todos/actions';
+import { useDispatch } from 'react-redux';
 
 export default function Header() {
-  return (
-    <div>
-                    <form
-                        className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
-                    >
-                        <img
-                            src={notes}
-                            className="w-6 h-6"
-                            alt="Add todo"
-                        />
-                        <input
-                            type="text"
-                            placeholder="Type your todo"
-                            className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
-                        />
-                        <button
-                            type="submit"
-                            className={`appearance-none w-8 h-8 
+    const dispatch = useDispatch();
+    const [input, setInput] = useState('');
+    const handleInput = (e) => {
+        setInput(e.target.value);
+    }
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(added(input));
+        setInput('');
+    }
+
+    const completeAllHandler = () => {
+        dispatch(allCompleted());
+    }
+
+    const clearHandler = () => {
+        dispatch(clearCompleted())
+    }
+    return (
+        <div>
+            <form
+                className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
+                onSubmit={submitHandler}
+            >
+                <img
+                    src={notes}
+                    className="w-6 h-6"
+                    alt="Add todo"
+                />
+                <input
+                    type="text"
+                    placeholder="Type your todo"
+                    className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+                    value={input}
+                    onChange={handleInput}
+                />
+                <button
+                    type="submit"
+                    className={`appearance-none w-8 h-8 
                             bg-[url(${plus})]
                         /></form>
                              bg-no-repeat bg-contain`}
-                        ></button>
-                    </form>
+                ></button>
+            </form>
 
-                    <ul className="flex justify-between my-4 text-xs text-gray-500">
-                        <li className="flex space-x-1 cursor-pointer">
-                            <img
-                                className="w-4 h-4"
-                                src={tick}
-                                alt="Complete"
-                            />
-                            <span>Complete All Tasks</span>
-                        </li>
-                        <li className="cursor-pointer">Clear completed</li>
-                    </ul>
-                </div>
-  )
+            <ul className="flex justify-between my-4 text-xs text-gray-500">
+                <li className="flex space-x-1 cursor-pointer"
+                onClick={completeAllHandler}
+                >
+                    <img
+                        className="w-4 h-4"
+                        src={tick}
+                        alt="Complete"
+                    />
+                    <span>Complete All Tasks</span>
+                </li>
+                <li className="cursor-pointer"
+                onClick={clearHandler}
+                >Clear completed</li>
+            </ul>
+        </div>
+    )
 }
